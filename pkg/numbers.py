40a26c9df50755d20bbc06463f19ded37cf69ea0
RitiKSiBJr <letter.ritik060@gmail.com>
2022-09-05 15:59:52 +0545

    changed

--- pkg/numbers.py
+++ pkg/numbers.py
@@ -2,15 +2,14 @@ import os
 def add_5_to_file():
     if os.path.exists("number.txt") == False:
         with open("number.txt","w") as file:
-            file.write("5")
+            file.write("0")
 
-    else:
-        with open("number.txt","r") as file:
-            a =  int(file.read())
-            a += 5
-    
-        with open("number.txt","w") as file:
-            file.write(str(a))
+    with open("number.txt","r") as file:
+        a =  int(file.read())
+
+    with open("number.txt","w") as file:
+        a += 5
+        file.write(str(a))
 
 def main():
     add_5_to_file()
